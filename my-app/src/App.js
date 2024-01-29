88c6ffeb9a9bb7071e11084a0561d3d6c964f428
Jean-Michel Uk <jmichel.uk@outlook.fr>
2024-01-29 09:00:53 +0100

    Basic Structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
 
 function App() {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <nav>
+        <div></div>
+        <ul>
+          <li>home</li>
+          <li>about</li>
+          <li>menu</li>
+          <li>reservations</li>
+          <li>order online</li>
+          <li>login</li>
+        </ul>
+      </nav>
+      <header></header>
+      <section>
+        <article></article>
+        <article></article>
+        <article></article>
+      </section>
+      <section>
+        <article></article>
+        <article></article>
+        <article></article>
+        <article></article>
+      </section>
+      <section></section>
+      <footer></footer>
+    </>
+  );
 }
 
 export default App;
